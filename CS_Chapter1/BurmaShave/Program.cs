097d078223d806e0c7f17773b8624fbe2f77a402
smguydish <smguydish@gmail.com>
2017-01-31 14:13:14 -0500

    BurmaShave Project

--- CS_Chapter1/BurmaShave/Program.cs
+++ CS_Chapter1/BurmaShave/Program.cs
@@ -10,6 +10,7 @@ namespace BurmaShave
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("   A Shave\n   That's Real\n   No cuts to heal\n   A Soothing\n   Velvet after-feel");
         }
     }
 }
